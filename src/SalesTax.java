c6a99567f92e8bde5efb230e64c7a01d9dad8559
IMCH222 <chukwuim@mail.uc.edu>
2023-06-04 11:24:44 -0400

    Initial commit

---
+++ src/SalesTax.java
@@ -0,0 +1,9 @@
+public class SalesTax
+{
+    public static void main(String[] args) {
+        int itemPrice = 0;
+        final double SALES_TAX = 0.05;
+        double finalPrice = 0;
+        finalPrice = 
+    }
+}
