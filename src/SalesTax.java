35bd4fc31b189d268d047144345035e4ea5df011
IMCH222 <chukwuim@mail.uc.edu>
2023-06-04 11:33:00 -0400

    Finshed task 1

--- src/SalesTax.java
+++ src/SalesTax.java
@@ -1,9 +1,13 @@
 public class SalesTax
 {
     public static void main(String[] args) {
-        int itemPrice = 0;
+        double itemPrice = 24.99;
         final double SALES_TAX = 0.05;
         double finalPrice = 0;
-        finalPrice = 
+        double taxPrice = 0;
+        taxPrice = itemPrice * SALES_TAX;
+        finalPrice = itemPrice + taxPrice;
+        System.out.println("The sales tax for your purchase is" + " " + "$"+ taxPrice + " " + "which makes you total:" + " " + " " + "$" + finalPrice);
+
     }
 }
